3e8d30d10cbe3fec286dbc7b3eb1d102fecfb112
Ruben <hello@rubengarciabri.me>
2021-04-27 21:48:52 +0100

    added responsive navbar and outsideclick functionality

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,20 +1,35 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useHistory } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { Button} from 'semantic-ui-react'
-import { FaHome, FaSignOutAlt, FaTrophy } from "react-icons/fa";
-import { MdAddCircle } from "react-icons/md";
-
+import { FaHome, FaSignOutAlt, FaTrophy, FaBars } from "react-icons/fa"
+import { MdAddCircle } from "react-icons/md"
+import { Menu } from 'semantic-ui-react'
+import { useOutsideClick } from '../utils/helpers'
 import { removeAuthedUser } from '../actions/authedUser'
 
+
 const Nav = ({ authedUser, dispatch }) => {
-  let history = useHistory();
+  const [activeSideMenu, setActiveSideMenu] = useState(false)
+
+  let history = useHistory()
+  const node = useRef()
 
   const handleLogout = () => {
     dispatch(removeAuthedUser())
     history.push('/')
   }
+
+  const onBarsClick = () => {
+    setActiveSideMenu(!activeSideMenu)
+  }
+
+  useOutsideClick(node, () => {
+    if (activeSideMenu === true) {
+      setActiveSideMenu(false)
+    }
+  })
 
   const activeColor = '#1CB0F6'
 
@@ -73,7 +88,53 @@ const Nav = ({ authedUser, dispatch }) => {
           </li>
         </ul>
         </div>
-        
+        <img src={authedUser.avatar} className='bars-userImg shadow-slim'/>
+        <FaBars className="bars" onClick={onBarsClick}/>     
+        {activeSideMenu === true 
+        ?
+          <ul ref={node} className='sideNav shadow-slim'>
+            <li className='sideNav__item'>
+              <NavLink
+              to='/home'
+              activeStyle={{
+              color: activeColor,
+              opacity: 1
+              }}>
+                <FaHome size={24} className='sideNav__item-icon'/>
+                <span>Home</span>              
+              </NavLink>
+            </li>
+            <li className='sideNav__item'>
+              <NavLink
+              to='/add'
+              activeStyle={{
+                color: activeColor,
+                opacity: 1
+              }}>
+                <MdAddCircle size={24} className='sideNav__item-icon'/>
+                <span>Create Poll</span>                
+              </NavLink>
+            </li>
+            <li className='sideNav__item'>
+              <NavLink
+              to='/leaderboard'
+              activeStyle={{
+                color: activeColor,
+                opacity: 1
+              }}>
+                <FaTrophy size={24} className='sideNav__item-icon'/>
+                <span>Leaderboard</span>               
+              </NavLink>
+            </li>
+            <li className='sideNav__item'>
+              <a className='sideNav__item-logout' onClick={handleLogout}>
+                <FaSignOutAlt size={24} className='sideNav__item-icon'/>
+                <span>Sign Out</span>               
+              </a>
+            </li>
+          </ul>
+        : null
+        }
       </div>
     </nav>
   )
--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,5 +1,22 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
+import { useEffect } from 'react'
 
 export function generateId() {
   return uuidv4()
 }
+
+export const useOutsideClick = (ref, callback) => {
+  const handleClick = e => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  });
+};
