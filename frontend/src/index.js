e37703b63eeedeb6407f276a88b7cf3d2b671b92
Ruben <hello@rubengarciabri.me>
2021-05-01 23:00:24 +0100

    moved node-sass to dependencies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'react-toastify/dist/ReactToastify.css'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
