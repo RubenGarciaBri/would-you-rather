2f9f26fd2e8384f40001b5734118ceabcbee6085
Ruben <hello@rubengarciabri.me>
2021-05-02 00:08:08 +0100

    serving html file from server in procution env

--- backend/server.js
+++ backend/server.js
@@ -18,3 +18,14 @@ app.use('/api/users', userRoutes)
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('frontend/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
