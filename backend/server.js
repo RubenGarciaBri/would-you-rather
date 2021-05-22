a76e7e73806b99fe7fa07e3b975d195b5307c3f5
Ruben <hello@rubengarciabri.me>
2021-05-22 19:51:29 +0100

    changed routing express

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
 
   // Express serve up index.html file if it doesn't recognize route
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
