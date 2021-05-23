fd83cae3625048b37ff60cd96907749424ab252a
Ruben <hello@rubengarciabri.me>
2021-05-23 22:26:37 +0100

    fixed merging issues with revert

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -3,48 +3,53 @@ import { Select, Button, Message } from 'semantic-ui-react'
 import { useHistory } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { setAuthedUser } from '../actions/authedUser'
+import { users } from '../utils/_DATA'
 
-const LoginForm = ({ dispatch, users }) => {
+const formUsers = [
+  {
+    text: users['Mike'].id,
+    value: users['Mike'].id,
+    image: {
+      avatar: true,
+      src: users['Mike'].avatar
+    }
+  },
+  {
+    text: users['Carol'].id,
+    value: users['Carol'].id,
+    image: {
+      avatar: true,
+      src: users['Carol'].avatar
+    }
+  },
+  {
+    text: users['Jennifer'].id,
+    value: users['Jennifer'].id,
+    image: {
+      avatar: true,
+      src: users['Jennifer'].avatar
+    }
+  },
+  {
+    text: users['Victor'].id,
+    value: users['Victor'].id,
+    image: {
+      avatar: true,
+      src: users['Victor'].avatar
+    }
+  }
+]
+
+const LoginForm = ({ dispatch }) => {
   const [user, setUser] = useState('')
   const [avatar, setAvatar] = useState('')
   const [submitFailed, setSubmitFailed] = useState(false)
 
   let history = useHistory()
 
-  const formUsers = [
-    {
-      text: users['Mike'].id,
-      value: users['Mike'].id,
-      image: {
-        avatar: true,
-        src: users['Mike'].avatar,
-      },
-    },
-    {
-      text: users['Carol'].id,
-      value: users['Carol'].id,
-      image: {
-        avatar: true,
-        src: users['Carol'].avatar,
-      },
-    },
-    {
-      text: users['Jennifer'].id,
-      value: users['Jennifer'].id,
-      image: {
-        avatar: true,
-        src: users['Jennifer'].avatar,
-      },
-    },
-    {
-      text: users['Victor'].id,
-      value: users['Victor'].id,
-      image: {
-        avatar: true,
-        src: users['Victor'].avatar,
-      },
-    },
-  ]
+  useEffect(() => {
+    
+  }, [])  
 
   const handleChange = (e) => {
     const target = e.target
@@ -95,10 +100,9 @@ const LoginForm = ({ dispatch, users }) => {
   )
 }
 
-function mapStateToProps({ authedUser, users }) {
+function mapStateToProps ({ authedUser }) {
   return {
-    authedUser,
-    users,
+    authedUser
   }
 }
 
--- frontend/src/utils/_DATA.js
+++ frontend/src/utils/_DATA.js
@@ -1,7 +1,39 @@
 import { generateId } from '../utils/helpers'
 import axios from 'axios'
 
-function formatPoll({ firstQuestion, secondQuestion, author }) {
+export let users = {
+  'Mike': {
+    id: 'Mike',
+    avatar: 'https://randomuser.me/api/portraits/men/49.jpg',
+    createdPolls: ['98c1e68-3320-4157-18w7-84c2d1e7zxf1v'],
+    answeredPolls: [],
+    points: 1
+  },
+  'Carol': {
+    id: 'Carol',
+    avatar: 'https://randomuser.me/api/portraits/women/2.jpg',
+    createdPolls: [],
+    answeredPolls: [],
+    points: 0
+  },
+  'Jennifer': {
+    id: 'Jennifer',
+    avatar: 'https://randomuser.me/api/portraits/women/3.jpg',
+    createdPolls: [],
+    answeredPolls: ['17w1v628-192q-1537-12x4-11a131j3tb6p'],
+    points: 1
+  },
+  'Victor': {
+    id: 'Victor',
+    avatar: 'https://randomuser.me/api/portraits/men/16.jpg',
+    createdPolls: [],
+    answeredPolls: [],
+    points: 0
+  }
+}
+
+
+function formatPoll ({ firstQuestion, secondQuestion, author }) {
   return {
     author,
     id: generateId(),
